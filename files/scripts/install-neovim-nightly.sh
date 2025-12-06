ef7e27afc3ffb65d8df4ec5992a38ad2a852bc25
Jose Fernandez <me@jrfernandez.com>
2025-12-05 20:20:25 -0700

    fix: install neovim to /usr instead of /usr/local
    /usr/local is symlinked to /var/usrlocal on ostree systems and
    doesn't persist in the immutable image layer.

--- files/scripts/install-neovim-nightly.sh
+++ files/scripts/install-neovim-nightly.sh
@@ -6,8 +6,12 @@ NVIM_URL="https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-
 
 curl -fsSL "$NVIM_URL" -o /tmp/nvim.tar.gz
 tar xzf /tmp/nvim.tar.gz -C /tmp
-cp -r /tmp/nvim-linux-x86_64/* /usr/local/
+
+# Install to /usr (not /usr/local which doesn't persist in ostree images)
+cp -r /tmp/nvim-linux-x86_64/bin/* /usr/bin/
+cp -r /tmp/nvim-linux-x86_64/lib/* /usr/lib/
+cp -r /tmp/nvim-linux-x86_64/share/* /usr/share/
 rm -rf /tmp/nvim.tar.gz /tmp/nvim-linux-x86_64
 
 # Verify installation
-/usr/local/bin/nvim --version
+/usr/bin/nvim --version
