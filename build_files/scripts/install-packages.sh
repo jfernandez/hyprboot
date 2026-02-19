f67a95af66266ef00ba1de26fb2a9418a90b470d
Jose Fernandez <me@jrfernandez.com>
2026-02-18 21:59:57 -0700

    feat: add intel wifi firmware for arrow lake support
    The BE201 card in Arrow Lake machines needs iwlwifi-bz firmware
    which isn't in the base image. Add both mvm and mld firmware
    packages to cover older and newer Intel wifi cards.

--- build_files/scripts/install-packages.sh
+++ build_files/scripts/install-packages.sh
@@ -73,7 +73,9 @@ dnf5 -y --setopt=max_parallel_downloads=20 install \
     pipewire-alsa \
     wtype \
     mesa-vulkan-drivers \
-    pinentry-qt
+    pinentry-qt \
+    iwlwifi-mvm-firmware \
+    iwlwifi-mld-firmware
 
 # Disable COPRs
 dnf5 -y copr disable atim/starship
